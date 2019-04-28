543050220dcef656294af7735426d0948cf37ea7
Mike <legoguy217@gmail.com>
2019-04-28 12:07:34 -0400

    Entire Project
    A simple program that allows you to add up amount of time to get a total.
    
    If you input "d4 h3 m2 s1" or "s356521" you'll get "4 Day(s) 3 Hour(s) 2 Minute(s) 1 Second(s)"

---
+++ TimeCruncher/TimeCruncher/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TimeCruncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(70, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "INPUT HERE";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(0, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "4 Day(s) 3 Hour(s) 2 Minute(s) 1 Second(s)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(142, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(Expecting Format: h1 s2 etc...)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(193, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Total:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 194);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Time Cruncher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TimeCruncher/TimeCruncher/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+/// <summary>
+/// Something I made just to streamline the process of counting my hours for work.
+/// </summary>
+
+namespace TimeCruncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "------";
+        }
+
+        private string originalText;
+
+        /// <summary>
+        /// Whenever text is added/deleted from the text box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (originalText != textBox1.Text)
+            {
+                originalText = textBox1.Text.ToLower();
+
+                //recalculate
+                if (textBox1.Text.Length != 0)
+                {
+                    label1.Text = CaculateTime(originalText);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a proper string format of the total amount of time spent.
+        /// Ex: Input: "d4 h3 m2 s1" or "s356521"
+        ///     Output: "4 Day(s) 3 Hour(s) 2 Minute(s) 1 Second(s)"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string CaculateTime(string text)
+        {
+            int minutes = 0, hours = 0, days = 0, seconds;
+
+            seconds = FindCoupledVariables('s');
+            minutes = FindCoupledVariables('m') + Multiple(seconds,60);
+            seconds %= 60;
+            hours = FindCoupledVariables('h') + Multiple(minutes,60);
+            minutes %= 60;
+            days = FindCoupledVariables('d') + Multiple(hours, 24);
+            hours %= 24;
+
+            return ((days != 0) ? days.ToString()+" Day(s) " : "")+
+                ((hours != 0) ? " "+hours.ToString()+" Hour(s) " : "")+
+                ((minutes != 0) ? " "+minutes.ToString()+" Minute(s) " : "")+
+                ((seconds != 0) ? " "+seconds.ToString()+" Second(s) " : "");
+        }
+
+        /// <summary>
+        /// Returns a value representing the amount of times the dividend goes into the amount
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="dividend"></param>
+        /// <returns></returns>
+        private int Multiple(int amount, int dividend)
+        {
+            int count = 0;
+
+            while (amount > 0)
+            {
+                amount -= dividend;
+
+                if (amount > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Returns the values associated with the targetChar (direct right of the char)
+        /// </summary>
+        /// <param name="targetChar"></param>
+        /// <returns></returns>
+        private int FindCoupledVariables(char targetChar)
+        {
+            int total = 0;
+            int startIndex = -1, currentIndex = 0;
+
+            while(currentIndex < originalText.Length)
+            {
+                if (startIndex == -1)
+                {
+                    //if we hit the target char
+                    if (originalText[currentIndex] == targetChar)
+                    {
+                        //Remeber where it was
+                        startIndex = currentIndex;
+                    }
+                }
+                else
+                {
+                    //If we've found a letter
+                    if ((currentIndex == originalText.Length-1 && ((int)originalText[currentIndex] >= 97 && (int)originalText[currentIndex] <= 122) || (int)originalText[currentIndex] == 32) || (((int)originalText[currentIndex] >= 97 && (int)originalText[currentIndex] <= 122) || (int)originalText[currentIndex] == 3))
+                    {
+                        total += Convert.ToInt32(originalText.Substring(startIndex+1,(currentIndex-1-startIndex)));
+                        startIndex = -1;
+                    }
+                }
+
+                currentIndex++;
+            }
+
+            return total;
+        }
+    }
+}
